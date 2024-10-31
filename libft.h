21904c77bf0d669db7fbb71ef8af20610049c516
AadelAferyad <animevineu@gmail.com>
2024-10-31 14:19:21 +0100

    update

--- libft.h
+++ libft.h
@@ -26,4 +26,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 #endif
