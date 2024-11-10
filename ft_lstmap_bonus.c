a2b9bcd0579579f757cbc0f380e841a22b3aaa91
aadelAferyad <animevineu@gmail.com>
2024-11-10 16:44:49 +0100

    last bonus func not tested yet!

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -15,19 +15,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	(void) lst;
-	(void) f;
-	(void) del;
-	/*t_list	*node;*/
-	/*t_list	*tmp;*/
-	/**/
-	/*if (!lst || !f || !del)*/
-	/*	return (NULL);*/
-	/*tmp = lst;*/
-	/*while (tmp)*/
-	/*{*/
-	/**/
-	/*	tmp = tmp->next;*/
-	/*}*/
-	return (NULL);
+	t_list	*new_list;
+	t_list	*node;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&new_list, del);
+			return (new_list);
+		}
+		ft_lstadd_back(&new_list, node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
