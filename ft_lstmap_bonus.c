5475c35c87f05f2feb7a60cfef7349d133f8ba3b
aadelAferyad <animevineu@gmail.com>
2024-11-11 12:34:00 +0100

    delete content if faild using func del

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*node;
+	void	*con;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		node = ft_lstnew(f(lst->content));
+		con = f(lst->content);
+		node = ft_lstnew(con);
 		if (!node)
 		{
+			del(con);
 			ft_lstclear(&new_list, del);
 			return (new_list);
 		}
