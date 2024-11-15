bc4fb203269902a197fbf60781aba0565e817fdb
AadelAferyad <animevineu@gmail.com>
2024-11-15 13:27:29 +0100

    print func

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aaferyad <aaferyad@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 18:02:36 by aaferyad          #+#    #+#             */
-/*   Updated: 2024/11/15 12:59:28 by aaferyad         ###   ########.fr       */
+/*   Updated: 2024/11/15 13:21:23 by aaferyad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,4 +68,6 @@ void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 int	ft_putchar(int c);
+int	ft_puts(const char *s);
+int	ft_putnbr(int n);
 #endif
